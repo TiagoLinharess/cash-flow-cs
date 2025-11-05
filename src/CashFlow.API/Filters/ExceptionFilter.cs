d2cf40b93ebe64d57cdb2ceae7395cc83cbbc0fa
Tiago Linhares <tiagolinharessouza@gmail.com>
2025-11-05 14:48:48 -0300

    implements localizable

--- src/CashFlow.API/Filters/ExceptionFilter.cs
+++ src/CashFlow.API/Filters/ExceptionFilter.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -39,7 +40,7 @@ namespace CashFlow.API.Filters
 
         private void ThrowUnkownError(ExceptionContext context)
         {
-            var errorResponse = new ResponseErrorJson("unkown error.");
+            var errorResponse = new ResponseErrorJson(ResourceErrorMessages.UNKNOWN_ERROR);
 
             context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
             context.Result = new ObjectResult(errorResponse);
--- src/CashFlow.Application/UseCases/Expenses/Create/RegisterExpenseValidator.cs
+++ src/CashFlow.Application/UseCases/Expenses/Create/RegisterExpenseValidator.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Communication.Requests;
+using CashFlow.Exception;
 using FluentValidation;
 
 namespace CashFlow.Application.UseCases.Expenses.Create
@@ -7,9 +8,9 @@ namespace CashFlow.Application.UseCases.Expenses.Create
     {
         public RegisterExpenseValidator()
         {
-            RuleFor(expense => expense.Title).NotEmpty().WithMessage("The title is required.");
-            RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage("The amount must be greater than zero.");
-            RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage("The payment type is invalid.");
+            RuleFor(expense => expense.Title).NotEmpty().WithMessage(ResourceErrorMessages.TITLE_REQUIRED);
+            RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
+            RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage(ResourceErrorMessages.INVALID_PAYMENT_TYPE);
         }
     }
 }
