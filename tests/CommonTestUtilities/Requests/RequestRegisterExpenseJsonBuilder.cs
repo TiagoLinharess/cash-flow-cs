97ac95ab895200f94e4224a1996f4e32d5f471c2
Tiago Linhares <tiagolinharessouza@gmail.com>
2025-11-06 10:12:31 -0300

    implements tests on validator

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestRegisterExpenseJsonBuilder
+    {
+        public static RequestRegisterExpenseJson Build()
+        {
+            return new Faker<RequestRegisterExpenseJson>()
+                .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+                .RuleFor(r => r.Description, faker => faker.Lorem.Paragraph())
+                .RuleFor(r => r.Date, faker => faker.Date.Past())
+                .RuleFor(r => r.Amount, faker => faker.Finance.Amount(1, 1000))
+                .RuleFor(r => r.PaymentType, faker => faker.PickRandom<CashFlow.Communication.Enums.PaymentType>());
+
+        }
+    }
+}
---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,68 @@
+﻿using CashFlow.Application.UseCases.Expenses.Create;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using Shouldly;
+
+namespace Validators.Tests.Expenses.Register
+{
+    public class RegisterExpenseValidatorTests
+    {
+        [Fact]
+        public void Success()
+        {
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+
+            var result = validator.Validate(request);
+
+            result.IsValid.ShouldBeTrue();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("    ")]
+        [InlineData(null)]
+        public void Error_Title_Invalid(string title)
+        {
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+            request.Title = title;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors.ShouldContain(e => e.ErrorMessage == ResourceErrorMessages.TITLE_REQUIRED);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Error_Amount_Invalid(decimal amount)
+        {
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+            request.Amount = amount;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors.ShouldContain(e => e.ErrorMessage == ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
+        }
+
+        [Fact]
+        public void Error_Payment_Type_Invalid()
+        {
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+            request.PaymentType = (CashFlow.Communication.Enums.PaymentType)999;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors.ShouldContain(e => e.ErrorMessage == ResourceErrorMessages.INVALID_PAYMENT_TYPE);
+        }
+    }
+}
