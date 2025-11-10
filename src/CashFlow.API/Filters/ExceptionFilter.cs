82709701e54c69f81c4ab041ae11832b63cd1119
Tiago Linhares <tiagolinharessouza@gmail.com>
2025-11-10 17:30:23 -0300

    implements open-closed

--- src/CashFlow.API/Filters/ExceptionFilter.cs
+++ src/CashFlow.API/Filters/ExceptionFilter.cs
@@ -16,35 +16,20 @@ namespace CashFlow.API.Filters
             }
             else
             {
-                ThrowUnkownError(context);
+                ThrowUnknownError(context);
             }
         }
 
         private void HandleProjectException(ExceptionContext context)
         {
-            if (context.Exception is ErrorOnValidationException errorOnValidationException)
-            {
-                var errorResponse = new ResponseErrorJson(errorOnValidationException.Errors);
-
-                context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-                context.Result = new BadRequestObjectResult(errorResponse);
-            }
-            else if (context.Exception is NotFoundException notFoundException)
-            {
-                var errorResponse = new ResponseErrorJson(notFoundException.Message);
-
-                context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
-                context.Result = new NotFoundObjectResult(errorResponse);
-            }
-            else
-            {
-                var errorResponse = new ResponseErrorJson(context.Exception.Message);
-                context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-                context.Result = new BadRequestObjectResult(errorResponse);
-            }
+            var cashFlowException = (CashFlowException)context.Exception;
+            var errorResponse = new ResponseErrorJson(cashFlowException.GetErrors());
+            
+            context.HttpContext.Response.StatusCode = cashFlowException.StatusCode;
+            context.Result = new ObjectResult(errorResponse);
         }
 
-        private void ThrowUnkownError(ExceptionContext context)
+        private void ThrowUnknownError(ExceptionContext context)
         {
             var errorResponse = new ResponseErrorJson(ResourceErrorMessages.UNKNOWN_ERROR);
 
