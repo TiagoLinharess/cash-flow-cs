3ec21b519eaf049e03353d5abcec30fd60177de8
Tiago Linhares <tiagolinharessouza@gmail.com>
2025-11-10 16:57:46 -0300

    implements localizable

--- src/CashFlow.Application/UseCases/Expenses/ReadAt/ReadAtExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/ReadAt/ReadAtExpenseUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Expenses.ReadAt;
@@ -25,6 +26,6 @@ internal class ReadAtExpenseUseCase : IReadAtExpenseUseCase
             return _mapper.Map<ResponseExpenseJson>(result);
         }
         
-        throw new NotFoundException("Despesa não encontrada");
+        throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
     }
 }
